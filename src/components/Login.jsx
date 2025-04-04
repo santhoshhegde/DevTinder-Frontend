5c9df2ec32e93a24a221aa30c39ef578545733a4
Developer <santhosh.hegde262000@gmail.com>
2025-04-04 12:07:52 +0530

    feat: SignUp

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,9 +5,13 @@ import { useDispatch } from "react-redux";
 import { addUser } from "../store/userSlice";
 import { useNavigate } from "react-router-dom";
 const Login = () => {
-  const [email, setEmail] = useState("agni@bhramlokha.in");
-  const [password, setPassword] = useState("Benki@anything/0");
+  const [email, setEmail] = useState("ganapathi@kilasa.in");
+  const [password, setPassword] = useState("Ladoo@anything/0");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [gender, setGender] = useState("");
   const [error, setError] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleLogin = async () => {
@@ -26,36 +30,99 @@ const Login = () => {
       setError(err.response.data);
     }
   };
+
+  const handleSingup = async () => {
+    try {
+      const res = await axios.post(
+        baseURL + API_ENDPOINTS.signUp,
+        { firstName, lastName, email, password },
+        { withCredentials: true }
+      );
+      navigate("/profile");
+      dispatch(addUser(res?.data?.data));
+      console.log(res?.data?.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="flex justify-center items-center mt-[10%]">
       <div className="card bg-base-100 w-96 shadow-sm">
         <div className="card-body">
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            className="input"
-            placeholder="Type here"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="text"
-            id="password"
-            className="input"
-            placeholder="Type here"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {error && (
-            <p className="m-auto text-l text-red-600 font-bold">{error}</p>
-          )}
-          <div className="card-actions justify-center">
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
-            </button>
-          </div>
+          <form onSubmit={(e) => e.preventDefault()}>
+            {!isLogin && (
+              <>
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  className="input"
+                  placeholder="Type here"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                />
+
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  className="input"
+                  placeholder="Type here"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                />
+                <label htmlFor="gender">Gender</label>
+                <input
+                  type="text"
+                  id="gender"
+                  className="input"
+                  placeholder="Type here"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  required
+                />
+              </>
+            )}
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              className="input"
+              placeholder="Type here"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              id="password"
+              className="input"
+              placeholder="Type here"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            {error && (
+              <p className="m-auto text-l text-red-600 font-bold">{error}</p>
+            )}
+            <div className="card-actions justify-center">
+              <button
+                className="btn btn-primary"
+                onClick={isLogin ? handleLogin : handleSingup}
+              >
+                {isLogin ? "Login" : "Signup"}
+              </button>
+            </div>
+            <p
+              className="text-center cursor-pointer"
+              onClick={() => setIsLogin(!isLogin)}
+            >
+              {isLogin ? "New user? SignUp" : " I have account? Login"}
+            </p>
+          </form>
         </div>
       </div>
     </div>
